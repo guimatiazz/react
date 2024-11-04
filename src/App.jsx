a1d8f85793786ebc0ccb4053911db35f5a7d9d77
guimatiazz <guilhermematiastere@gmail.com>
2024-11-04 17:29:01 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Saudacao from './componentes/saudacao'
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Saudacao.jsx
@@ -0,0 +1,15 @@
+const Saudacao = (preço) ={
+    const data = new Date();
+    const horaAtual = data.getHoras();
+    const usario = props.usario;
+    const 
+
+}
+
+echo "# react" >> README.md 
+git init 
+git add .
+git commit -m "primeiro commit" 
+git branch -M main 
+git remote add origin https://github.com/guimatiazz/react.git
+ git push -u origin main
